f4a9d6f810177b74fef339565264a36d9acc2b67
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-09-26 20:50:33 -0400

    Time: 33 ms (22.97%), Space: 44.4 MB (86.44%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -1,12 +1,12 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
              
-		 Set<Integer> st = new HashSet<Integer>();
+		Map<Integer,Integer> st = new HashMap<Integer,Integer>();
 		 
 		 for(int i=0;i<candyType.length;i++)
-			 if(!st.contains(candyType[i]))
-				 st.add(candyType[i]);
-	      return Math.min(st.size(), candyType.length/2);  
+			 if(!st.containsKey(candyType[i]))
+				 st.put(candyType[i], 1);
+	      return Math.min(st.keySet().size(), candyType.length/2);  
 	      
 	    }
         
