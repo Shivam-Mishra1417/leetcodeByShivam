3ddfd0a58fb374bfd716980d1082be3b7211bb48
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-09-29 22:25:50 -0400

    Time: 5 ms (21.47%), Space: 43.7 MB (15.65%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,25 +1,41 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-           ArrayList<Integer> list = new ArrayList<>();
-	       HashMap<Integer,Boolean> map = new HashMap<>();
+          // ArrayList<Integer> list = new ArrayList<>();
+	    //    HashMap<Integer,Boolean> map = new HashMap<>();
 	       
-	       for(int i=0;i<nums1.length;i++) {
-               if(!map.getOrDefault(nums1[i],false))
-	    	   map.put(nums1[i], true);
+	    //    for(int i=0;i<nums1.length;i++) {
+        //        if(!map.getOrDefault(nums1[i],false))
+	    // 	   map.put(nums1[i], true);
+	    //    }
+	       
+	    //    for(int i=0;i<nums2.length;i++) {
+	    // 	   if(map.getOrDefault(nums2[i], false)) {
+	    // 		   list.add(nums2[i]);
+	    // 		   map.put(nums2[i], false);
+	    // 	   }
+	    //    }
+	        
+        //     int [] arr = new int[list.size()];
+        //     for(int i=0;i<list.size();i++){
+        //         arr[i] = list.get(i);
+        //     }
+	        
+        //     return arr;
+
+         HashSet<Integer> n1= new HashSet<Integer>();
+	       HashSet<Integer> n2= new HashSet<Integer>();
+	       
+	       for(int i=0;i<nums1.length || i<nums2.length;i++) {
+	    	   
+	    	   if(i<nums1.length) n1.add(nums1[i]);
+	    	   if(i<nums2.length) n2.add(nums2[i]);
+	    	   
 	       }
 	       
-	       for(int i=0;i<nums2.length;i++) {
-	    	   if(map.getOrDefault(nums2[i], false)) {
-	    		   list.add(nums2[i]);
-	    		   map.put(nums2[i], false);
-	    	   }
-	       }
-	        
-            int [] arr = new int[list.size()];
-            for(int i=0;i<list.size();i++){
-                arr[i] = list.get(i);
-            }
-	        
-            return arr;
+	       n1.retainAll(n2);
+	       return n1.stream()
+	    		   .mapToInt(Integer::intValue)
+	    		   .toArray();  
+
 	    }
 }
