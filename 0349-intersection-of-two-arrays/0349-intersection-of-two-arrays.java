7f724e840b01fa92a073fd47762bc14e22a54dec
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-09-29 22:41:32 -0400

    Time: 2 ms (96%), Space: 43.5 MB (35.25%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
          // ArrayList<Integer> list = new ArrayList<>();
-	       HashMap<Integer,Boolean> map = new HashMap<>();
+	      /* HashMap<Integer,Boolean> map = new HashMap<>();
 	       HashMap<Integer,Boolean> map1 = new HashMap<>();
 	       for(int i=0;i<nums1.length;i++) {
                if(!map.getOrDefault(nums1[i],false))
@@ -21,26 +21,28 @@ class Solution {
                  arr[j++] = i;
              }
 	        
-            return arr;
+            return arr;*/
 
-        //  HashSet<Integer> n1= new HashSet<Integer>();
-	    //    HashSet<Integer> n2= new HashSet<Integer>();
+         HashSet<Integer> n1= new HashSet<Integer>();
+	     HashSet<Integer> n2= new HashSet<Integer>();
 	       
-	    //    for(int i=0;i<nums1.length || i<nums2.length;i++) {
-	    	   
-	    // 	   if(i<nums1.length) n1.add(nums1[i]);
-	    // 	   if(i<nums2.length) n2.add(nums2[i]);
-	    	   
-	    //    }
+	       for(int i=0;i<nums1.length;i++) {
+	    	n1.add(nums1[i]);	    	   
+	       }
+
+           for(int i=0;i<nums2.length;i++){
+               if(n1.contains(nums2[i]))
+               n2.add(nums2[i]);
+           }
 	       
-	    //    n1.retainAll(n2);
-	    //    int [] arr = new int[n1.size()];
-        //    int j=0;
-        //     for(int i:n1){
-        //         arr[j++] = i;
-        //     }
+	      
+	       int [] arr = new int[n2.size()];
+           int j=0;
+            for(int i:n2){
+                arr[j++] = i;
+            }
 	        
-        //     return arr;  
+            return arr;  
 
 	    }
 }
