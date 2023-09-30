1b5cd76119a9873c6eabb014f881400343a82c41
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-09-29 22:39:55 -0400

    Time: 3 ms (52.34%), Space: 44 MB (5.62%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,45 +1,46 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-          // ArrayList<Integer> list = new ArrayList<>();
-	    //    HashMap<Integer,Boolean> map = new HashMap<>();
-	       
-	    //    for(int i=0;i<nums1.length;i++) {
-        //        if(!map.getOrDefault(nums1[i],false))
-	    // 	   map.put(nums1[i], true);
-	    //    }
-	       
-	    //    for(int i=0;i<nums2.length;i++) {
-	    // 	   if(map.getOrDefault(nums2[i], false)) {
-	    // 		   list.add(nums2[i]);
-	    // 		   map.put(nums2[i], false);
-	    // 	   }
-	    //    }
-	        
-        //     int [] arr = new int[list.size()];
-        //     for(int i=0;i<list.size();i++){
-        //         arr[i] = list.get(i);
-        //     }
-	        
-        //     return arr;
-
-         HashSet<Integer> n1= new HashSet<Integer>();
-	       HashSet<Integer> n2= new HashSet<Integer>();
-	       
-	       for(int i=0;i<nums1.length || i<nums2.length;i++) {
-	    	   
-	    	   if(i<nums1.length) n1.add(nums1[i]);
-	    	   if(i<nums2.length) n2.add(nums2[i]);
-	    	   
+         // ArrayList<Integer> list = new ArrayList<>();
+	       HashMap<Integer,Boolean> map = new HashMap<>();
+	       HashMap<Integer,Boolean> map1 = new HashMap<>();
+	       for(int i=0;i<nums1.length;i++) {
+               if(!map.getOrDefault(nums1[i],false))
+	    	   map.put(nums1[i], true);
 	       }
 	       
-	       n1.retainAll(n2);
-	       int [] arr = new int[n1.size()];
-           int j=0;
-            for(int i:n1){
-                arr[j++] = i;
-            }
+	       for(int i=0;i<nums2.length;i++) {
+	    	   if(map.getOrDefault(nums2[i], false)) {
+	    		  
+	    		   map1.put(nums2[i], false);
+	    	   }
+	       }
 	        
-            return arr;  
+            int [] arr = new int[map1.keySet().size()];
+            int j=0;
+             for(int i:map1.keySet()){
+                 arr[j++] = i;
+             }
+	        
+            return arr;
+
+        //  HashSet<Integer> n1= new HashSet<Integer>();
+	    //    HashSet<Integer> n2= new HashSet<Integer>();
+	       
+	    //    for(int i=0;i<nums1.length || i<nums2.length;i++) {
+	    	   
+	    // 	   if(i<nums1.length) n1.add(nums1[i]);
+	    // 	   if(i<nums2.length) n2.add(nums2[i]);
+	    	   
+	    //    }
+	       
+	    //    n1.retainAll(n2);
+	    //    int [] arr = new int[n1.size()];
+        //    int j=0;
+        //     for(int i:n1){
+        //         arr[j++] = i;
+        //     }
+	        
+        //     return arr;  
 
 	    }
 }
