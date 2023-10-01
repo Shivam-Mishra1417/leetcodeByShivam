4b709e2fc3b0ce122313d0c0a8d7d2afb01960f8
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-09-30 21:25:01 -0400

    Time: 59 ms (7.38%), Space: 44.5 MB (30.04%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String reverseWords(String s) {
+        String res="", tmp=" "; boolean one=true;
+        for(int i=0;i<s.length();i++){
+            int index = s.length()-1;
+
+            if(!s.substring(index-i,index-i+1).equals(" "))
+                tmp +=s.substring(index-i,index-i+1);
+            else
+             {
+                System.out.println(tmp);
+                // if(one){
+                //  res = tmp+ res;
+                //  one=false;   
+                // }
+                //  else
+                 res = tmp+ res;
+
+                 tmp=" ";
+
+             }
+        }
+         res = tmp.substring(1)+ res;
+        return res;
+    }
+}
