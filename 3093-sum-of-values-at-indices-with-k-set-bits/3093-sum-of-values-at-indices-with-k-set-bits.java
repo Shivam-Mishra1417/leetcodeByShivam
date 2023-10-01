36b7bba8e8daa03f0f1091068298f84621b81e5b
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-09-30 22:38:36 -0400

    Time: 1 ms (100%), Space: 43.1 MB (76.23%) - LeetHub

--- 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.java
+++ 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.java
@@ -3,9 +3,8 @@ class Solution {
        // boolean [] bo = new boolean[nums.size()];
         int sum=0;
         for(int i=0;i<nums.size();i++){
-            String s = Integer.toBinaryString(i);
-            int x = s.replace("0","").length();
-            if(x==k) sum+=nums.get(i);
+            
+            if(Integer.bitCount(i)==k) sum+=nums.get(i);
             //bo[i] = true;
         }
 
