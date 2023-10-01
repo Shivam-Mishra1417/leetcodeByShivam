f633b4bf1417e315a2e81da71002468ab78e2e53
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-09-30 21:59:14 -0400

    Time: 7 ms (6.52%), Space: 44.4 MB (8.55%) - LeetHub

--- 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.java
+++ 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.java
@@ -1,15 +1,26 @@
+// class Solution {
+//     public String maximumOddBinaryNumber(String s) {
+//         String res="";
+//         for(int i=0;i<s.length();i++){
+//           if(s.charAt(i)=='0'){
+//               res = res+ "0";
+//           }
+//           else{
+//               res = "1"+ res;
+//           }
+//         }
+       
+//         return res.substring(1)+"1";
+//     }
+// }
+
 class Solution {
     public String maximumOddBinaryNumber(String s) {
-        String res="";
-        for(int i=0;i<s.length();i++){
-          if(s.charAt(i)=='0'){
-              res = res+ "0";
-          }
-          else{
-              res = "1"+ res;
-          }
+        int ones = 0, zeroes = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '1') ones++;
+            else zeroes++;
         }
-       
-        return res.substring(1)+"1";
+        return "1".repeat(ones - 1) + "0".repeat(zeroes) + (ones > 0 ? "1" : "");
     }
 }
