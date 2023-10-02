882911c5cfacc233e359a70ee615e4d4614f8a03
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-10-01 23:14:54 -0400

    Time: 18 ms (18.53%), Space: 44.2 MB (41.35%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,14 +1,27 @@
 class Solution {
     public boolean winnerOfGame(String colors) {
-        int a=0,b=0;
+        int a=0,b=0,tmp=1;
         String al = "AAA", bo = "BBB";
+        char prev = colors.charAt(0);
+        for(int i=1;i<colors.length();i++){
 
-        for(int i=0;i<colors.length()-2;i++){
-            String s = colors.substring(i,i+3);
-            if(s.equals(al)) a++;
-            if(bo.equals(s)) b++;
+            if(colors.charAt(i)==prev){
+                tmp++;
+                 if(tmp>=3){
+                if(prev=='A') a++;
+                else b++;
+            }
+            }
+            else
+                tmp=1;
+            
+            prev = colors.charAt(i);
+            // String s = colors.substring(i,i+3);
+            // if(s.equals(al)) a++;
+            // if(bo.equals(s)) b++;
           //  System.out.println(s);
         }
+        System.out.println(a+"  "+b);
         if(a>b) return true;
         return false;
     }
