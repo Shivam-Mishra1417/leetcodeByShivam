20bbe1276a3ffc44d3f9f2ef467b2c2be8791bc6
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-10-02 14:41:09 -0400

    Time: 0 ms (100%), Space: 39.2 MB (47.17%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -5,13 +5,13 @@ class Solution {
     }
 
     public static boolean bs(double num, int l , int r){
-       System.out.println(l+"  "+r+"   "+num);
+      // System.out.println(l+"  "+r+"   "+num);
         if(l>r) return false;
 
         int m = (int)Math.ceil(l+(r-l)/2);
 
          double x = Math.pow(m,2);
-        System.out.println(m+"  "+x+"   "+num);
+       // System.out.println(m+"  "+x+"   "+num);
         if(x==num) return true;
         if(x>num) return bs(num, l, m-1);
         else return bs(num, m+1,r);
