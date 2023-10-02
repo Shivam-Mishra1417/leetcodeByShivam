4f9177efa7d1521a7285a37af634bac353ad787f
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-10-02 11:37:47 -0400

    Time: 1 ms (100%), Space: 41.4 MB (5.42%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public char findTheDifference(String s, String t) {
         
         char[] s1 = s.toCharArray();
@@ -11,5 +11,18 @@ class Solution {
         }
 
         return t1[t1.length-1];
+    }
+}*/
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int f[] = new int[26];
+        for(char c : s.toCharArray())
+            f[c - 'a']--;
+        for(char c : t.toCharArray())
+            f[c - 'a']++;
+        int i = 0;
+        for(; i < 26; i++)
+            if(f[i] == 1) break;
+        return (char)(i + 'a');
     }
 }
