9c777d12ee841c1d12268b28ce5b6cd099456bbb
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-10-02 12:38:45 -0400

    Time: 3 ms (78.4%), Space: 43.6 MB (75.35%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        
+        int[] mag = new int[26];
+
+        for(int i=0;i<magazine.length();i++){
+            mag[magazine.charAt(i) - 'a']++;     
+        }
+
+        for(int i=0;i<ransomNote.length();i++){
+            if(mag[ransomNote.charAt(i) - 'a']<1)
+            return false;
+            else
+            mag[ransomNote.charAt(i) - 'a']--;
+        }
+        return true;
+    }
+}
