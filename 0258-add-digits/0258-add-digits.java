0d5c322587f8a1210ac8cea7dc4b0a05f9e57f59
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-10-03 14:05:38 -0400

    Time: 13 ms (88.56%), Space: 39.8 MB (25.2%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int addDigits(int num) {
+       // System.out.println(Math.log10(10));
+        
+        while(Math.floor(Math.log10(num))+1 > 1){
+          num = sum(num);
+          System.out.println(" num = "+num);
+        }
+
+        return num;
+    }
+
+    public int sum(int num){
+        int s=0;
+
+        while(num>0){
+            s+=num%10;
+            num=num/10;
+        }
+        return s;
+    }
+}
