c5feeae2561c06cd99397b6123195dcb0595a846
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-10-03 03:43:29 -0400

    Time: 1 ms (99.95%), Space: 44.5 MB (52.39%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,15 +1,25 @@
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         HashSet<Integer> set = new HashSet<>();
+//         for(int x:nums){
+//             if(set.contains(x))
+//             set.remove(x);
+//             else
+//             set.add(x);
+//         }
+//         for(int x:set)
+//         return x;
+
+//         return 0;
+//     }
+// }
+
 class Solution {
     public int singleNumber(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int x:nums){
-            if(set.contains(x))
-            set.remove(x);
-            else
-            set.add(x);
+        int ans=0; //since XOR with 0 returns same number 
+        for(int i=0; i<nums.length; i++){
+            ans ^= nums[i];  // ans = (ans) XOR (array element at i) 
         }
-        for(int x:set)
-        return x;
-
-        return 0;
+        return ans;    
     }
 }
