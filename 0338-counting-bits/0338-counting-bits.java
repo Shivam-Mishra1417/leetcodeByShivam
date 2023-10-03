847fd49db3c4de96f384bb37c90911adf82400ce
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-10-02 23:07:24 -0400

    Time: 2 ms (84.15%), Space: 46.5 MB (44.53%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -3,6 +3,7 @@ class Solution {
         int[] res = new int[n+1];
         for(int i=0;i<=n;i++)
         res[i] = Integer.bitCount(i);
+        //System.out.println(Arrays.toString(res));
         return res;
     }
 }
