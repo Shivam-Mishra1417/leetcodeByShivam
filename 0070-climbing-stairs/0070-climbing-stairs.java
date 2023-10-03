b5a497168322327d295e6420394200333a1d4b4b
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-10-03 12:54:57 -0400

    Time: 0 ms (100%), Space: 38.9 MB (84.53%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    int[] res = new int[46];
+    public int climbStairs(int n) {
+        res[1]=1;
+        res[2] = 2;
+        for(int i=3;i<46;i++){
+            res[i] = res[i-1]+res[i-2];
+        }
+      return res[n];
+    }
+}
