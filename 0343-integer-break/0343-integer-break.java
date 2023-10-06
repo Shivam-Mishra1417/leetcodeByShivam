2bd48933aa177faf6821c13a87eb9ddc5d1a2aba
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-10-05 22:42:59 -0400

    Time: 0 ms (100%), Space: 39.6 MB (17.26%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,17 +1,25 @@
 class Solution {
     public int integerBreak(int n) {
-        List<Integer> list = new ArrayList<Integer>();
-        list.add(0);
-        list.add(0);
-        list.add(1);
-        list.add(2);
-        list.add(4);
-        list.add(6);
-        list.add(9);
-        if(n<7) return list.get(n);
+        // List<Integer> list = new ArrayList<Integer>();
+        // list.add(0);
+        // list.add(0);
+        // list.add(1);
+        // list.add(2);
+        // list.add(4);
+        // list.add(6);
+        // list.add(9);
+        // if(n<7) return list.get(n);
+        // return Math.max(2*integerBreak(n-2), 3*integerBreak(n-3));
+
+            if(n==2) return 1;
+            if(n==3) return 2;
        
-        
-        
-     return Math.max(2*integerBreak(n-2), 3*integerBreak(n-3));
+            if(n%3==0)
+            return (int)Math.pow(3,n/3); 
+            else if(n%3==1)
+            return (int)(Math.pow(3,Math.floor(n/3)-1)*4);
+            else return(int)(Math.pow(3,Math.floor(n/3) )*2);
+       
+
     }
 }
