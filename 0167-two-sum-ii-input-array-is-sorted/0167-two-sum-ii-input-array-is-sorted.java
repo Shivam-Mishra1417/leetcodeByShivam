17c343126be414148785a20b6d8722a17414ea31
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-11-01 13:34:57 -0400

    Time: 2 ms (86.79%), Space: 45.6 MB (26.95%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,19 +1,33 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int[] res= new int[2];
-        for(int i=0;i<numbers.length;i++){
-            
+       
+       int l=0,r=numbers.length-1;
+
+       while(l<r){
+
+           int sum = numbers[l] + numbers[r];
+
+           if(sum==target){
+               res[0] = l+1;
+               res[1] = r+1;
+           }
+
+           if(sum>target) r--;
+           else l++; 
+       }
+       
+       /* for(int i=0;i<numbers.length;i++){
             int tmp = bs(numbers, i+1,numbers.length-1,target-numbers[i]);
             if(tmp!=1001){
                 res[0] = i+1;
                 res[1] = tmp+1;
-            }
-        
-        }
+            } 
+        }*/
         return res;
     }
 
-    public int bs(int[] nums, int l, int r, int t){
+   /* public int bs(int[] nums, int l, int r, int t){
 
         int m = l+(r-l)/2;
         if(l>r) return 1001;
@@ -24,5 +38,5 @@ class Solution {
         else
         return bs(nums, m+1,r,t);
 
-    }
+    }*/
 }
