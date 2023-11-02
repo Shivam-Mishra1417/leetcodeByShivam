88a0475d2983cba40580434b10ae5289f255f310
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-11-02 15:24:21 -0400

    Time: 34 ms (67.95%), Space: 56.2 MB (37.73%) - LeetHub

--- 3104-happy-students/3104-happy-students.java
+++ 3104-happy-students/3104-happy-students.java
@@ -2,10 +2,11 @@ class Solution {
     public int countWays(List<Integer> nums) {
 
         Collections.sort(nums);
-        nums.add(nums.size()+1);
+        nums.add(nums.size()+1);// to check if all students can be selected
         int ans=0;
-        if(nums.get(0)!=0) ans=1;
+        if(nums.get(0)!=0) ans=1; // If 0 is not in the list then we can have a case where we do not select any student beacuse of condition 2.
         
+        // for each inex we check if we select students till that index, is condition 2 being met because all other nums[i] should be less than selected students ie. i. and also length should be more for each nums[i].
         for(int i=0;i<nums.size()-1;i++){
             if(nums.get(i+1)> i+1 && nums.get(i)<i+1)
                 ans++;
