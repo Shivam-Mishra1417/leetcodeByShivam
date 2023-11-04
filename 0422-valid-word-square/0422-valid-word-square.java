21f138c10212404f7fd98a743d7c18fa35fbaf08
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-11-04 15:46:01 -0400

    Time: 36 ms (5.88%), Space: 45.1 MB (5.43%) - LeetHub

---
+++ 0422-valid-word-square/0422-valid-word-square.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean validWordSquare(List<String> words) {
+        
+        for(int i=0;i<words.size();i++){
+            String a = words.get(i);
+            String b = "";
+            for(int j=0;j<words.size();j++){
+                if(i<words.get(j).length()){
+                   b += ""+words.get(j).charAt(i); 
+                }
+                   
+            }
+            if(!a.equals(b)) {
+            System.out.println(a+"  "+b);
+            return false;
+            }
+        }
+        return true;
+    }
+}
