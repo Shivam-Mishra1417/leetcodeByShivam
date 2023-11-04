a459edd3b564ed32af533457a5e5fad10bc3573d
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-11-04 16:10:24 -0400

    Time: 0 ms (100%), Space: 40.2 MB (50.54%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
@@ -1,12 +1,10 @@
 class Solution {
     public int countSegments(String s) {
         int ct=0;
-        boolean flag=false;
-        s = " "+s;
+        s = " "+s; //to handle cases when there is no space at all like "Hello"
         for(int i=0;i<s.length()-1;i++){
            if(s.charAt(i) ==' ' && s.charAt(i+1)!=' '){
             ct++;    
-            flag = true;
            }
         }
         return ct;
