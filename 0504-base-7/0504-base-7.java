983eab252610466eda5a9a18d286cb4311eb4ace
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2023-11-04 20:22:13 -0400

    Time: 0 ms (100%), Space: 39.4 MB (96.23%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -1,17 +1,15 @@
 class Solution {
-    public String convertToBase7(int num) {
-        String ans="";
-        if(num==0) return num+"";
-        boolean neg=false;
-        if(num <0){
-            neg = true;
-            num = num*(-1);
-        }
-        
-        while(num!=0){
-            ans = num%7+""+ans;
-            num = num/7;
-        }
-        return (neg)? "-"+ans:ans;
+    public String convertToBase7(int num) 
+    {
+        int base=1;
+        int ans=0;
+        while(num!=0)
+        {
+            int rem=num%7;
+            ans+=base*rem;
+            base*=10;
+            num/=7;
+        }    
+        return Integer.toString(ans);
     }
 }
