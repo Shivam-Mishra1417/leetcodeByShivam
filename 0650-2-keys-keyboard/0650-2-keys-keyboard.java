b20a9de9f6dcb8acfce7a49c0d1475ba9384792a
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2024-06-01 16:24:35 -0400

    Time: 0 ms (100%), Space: 40.4 MB (63.5%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,16 +1,14 @@
 class Solution {
     public int minSteps(int n) {
         if(n==1) return 0;
-        int sum=0;
-        int ct=2;
+        int sum=0, ct=2;
         while(n>ct){
             if(n%ct==0){
                 n = n/ct;
                 sum+=ct;
             }
-            else{
+            else
                 ct++;
-            }
         }
         return sum+ct;
     }
