14a4ba62e09ea136a845fdad69188703da65301e
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2024-06-06 17:43:58 -0400

    Time: 4 ms (67.97%), Space: 76.3 MB (98.56%) - LeetHub

--- 3373-maximum-prime-difference/3373-maximum-prime-difference.java
+++ 3373-maximum-prime-difference/3373-maximum-prime-difference.java
@@ -8,16 +8,13 @@ class Solution {
         for(int i=2;i<=10;i++){
             for(int j=2;i*j<=100;j++)
                 pn[i*j]=false;
-        }
-        
+        } 
         for(int i=0;i<pn.length;i++)
             if(pn[i])
              prime.add(i);
     }
-    public int maximumPrimeDifference(int[] nums) {
-        
 
-       // System.out.println(prime);
+    public int maximumPrimeDifference(int[] nums) {
         int l=0,r=0;
         for(int i=0;i<nums.length;i++)
             if(prime.contains(nums[i]))
