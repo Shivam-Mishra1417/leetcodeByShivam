8d1435f81bd47472aa1a370f5d0e3b85e7b3f804
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2024-06-06 17:32:45 -0400

    Time: 1 ms (100%), Space: 42.4 MB (84.89%) - LeetHub

---
+++ 3361-latest-time-you-can-obtain-after-replacing-characters/3361-latest-time-you-can-obtain-after-replacing-characters.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String findLatestTime(String s) {
+        char[] nc = s.toCharArray();
+      
+            if(nc[0]=='?'){
+                if(nc[1]>'1' && nc[1]!='?')
+                    nc[0]='0';
+                else 
+                    nc[0] ='1';
+            }
+
+            if(nc[1]=='?'){
+                if(nc[0]=='0')
+                    nc[1]='9';
+                else
+                    nc[1]='1';
+            }
+
+            if(nc[3]=='?'){
+                nc[3]='5';
+            }
+
+            if(nc[4]=='?')
+                nc[4]='9';
+        return new String(nc);
+    }
+}
