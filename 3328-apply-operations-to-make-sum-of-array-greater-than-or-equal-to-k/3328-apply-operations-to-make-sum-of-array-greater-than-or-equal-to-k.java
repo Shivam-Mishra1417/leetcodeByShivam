85fdaa568a79a4f05c4f0b158b385668e649b32f
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2024-06-09 20:52:08 -0400

    Time: 1 ms (70.21%), Space: 40.5 MB (67.73%) - LeetHub

--- 3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.java
+++ 3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.java
@@ -3,7 +3,7 @@ class Solution {
         
         if(k==1) return 0;
         int max=Integer.MAX_VALUE,curr=0;
-        for(int i= 1;i<=k/2;i++){
+        for(int i= 1;i<=(int)Math.sqrt(k);i++){
             int tmp = (int)Math.ceil((double)(k-i)/i);
             curr = (i-1) + tmp ;
             if(curr<max)
