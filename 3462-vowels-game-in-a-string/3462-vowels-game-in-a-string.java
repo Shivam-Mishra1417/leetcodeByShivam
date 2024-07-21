f259d837e729fb5a652605e773da73fc816b267a
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2024-07-20 22:51:53 -0400

    Time: 4 ms (100%), Space: 45.4 MB (100%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -2,13 +2,10 @@ class Solution {
     public boolean doesAliceWin(String s) {
         
         char[] sar = s.toCharArray();
-        int ct=0;
-        boolean lastcharvowel = false;
         for(int i=0;i<sar.length;i++){
             if(sar[i]=='a' || sar[i]=='e' || sar[i]=='i' ||sar[i]=='o' ||sar[i]=='u' )
                 return true;
         } 
-        // if(ct>0) return true;
         return false;
 
     }
