229376d60620fdd525da5e02b2e86268d12e20c6
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2024-07-24 23:42:43 -0400

    Time: 1020 ms (87.7%), Space: 0B (100%) - LeetHub

---
+++ 1480-movie-rating/1480-movie-rating.sql
@@ -0,0 +1,54 @@
+# Write your MySQL query statement below
+-- -- select name as results from users where 
+-- -- user_id = (
+-- -- select user_id
+-- -- from movierating 
+-- -- group by 1
+-- -- order by count(*) desc
+-- -- limit 1 )
+
+-- -- union
+
+-- select movie_id, avg(rating)
+-- from movierating
+-- where year(created_at)=2020 and month(created_at)=02
+-- group by movie_id
+-- order by 2
+
+
+
+
+with movie as(select 
+u.name,m.title, mr.rating, mr.created_at
+from movierating mr, users u, movies m
+where mr.movie_id = m.movie_id
+and mr.user_id = u.user_id order by mr.movie_id)
+(
+select name as results
+from movie
+group by name
+order by count(*) desc, name
+limit 1
+)
+union all
+(
+select title as results
+from movie
+where created_at between '2020-02-01' and '2020-02-29'
+group by title
+order by avg(rating) desc, title
+limit 1
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
