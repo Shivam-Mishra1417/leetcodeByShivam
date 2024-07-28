7f12999afa3bc625c74bb3f31ffb685a4c18f9c1
Shivam Mishra <100092728+Shivam-Mishra1417@users.noreply.github.com>
2024-07-27 22:45:04 -0400

    Time: 24 ms (100%), Space: 40.6 MB (100%) - LeetHub

--- 3507-find-the-count-of-numbers-which-are-not-special/3507-find-the-count-of-numbers-which-are-not-special.java
+++ 3507-find-the-count-of-numbers-which-are-not-special/3507-find-the-count-of-numbers-which-are-not-special.java
@@ -1,27 +1,29 @@
 class Solution {
     public int nonSpecialCount(int l, int r) {
-        int ct=0;
-        for(int i=(int)Math.ceil(Math.sqrt(l));i<=(int)Math.sqrt(r);i++){
-            if(isPrime(i))
+        int ct = 0;
+        for (int i = (int) Math.ceil(Math.sqrt(l)); i <= (int) Math.sqrt(r); i++) {
+            if (isPrime(i))
                 ct++;
         }
 
-        return r-l+1-ct;
+        return r - l + 1 - ct;
     }
-    private static boolean isPrime(int n){
-         if (n <= 1) return false; // 0 and 1 are not prime numbers
-        if (n <= 3) return true; // 2 and 3 are prime numbers
 
-        // Check for even numbers and multiples of 3
-        if (n % 2 == 0 || n % 3 == 0) return false;
+    private static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+        if (n <= 3)
+            return true;
 
-        // Check for primes using 6k +/- 1 rule
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+
         for (int i = 5; i * i <= n; i += 6) {
-            if (n % i == 0 || n % (i + 2) == 0) return false;
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
         }
 
         return true;
     }
-
 
 }
